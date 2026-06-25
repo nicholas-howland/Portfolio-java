d907feb54145d3c00bdaddae24ce92f2e9534fe1
Nicholas Howland <94145193+nicholas-howland@users.noreply.github.com>
2026-06-25 16:54:52 -0700

    Added comments
    added in comments

--- weather-data-parser/WeatherData.java
+++ weather-data-parser/WeatherData.java
@@ -14,9 +14,22 @@ public class weatherData{
 // set up vars
         String line = fscaner.nextLine();
         String[] val = line.split(",");   
+        int lineNumber = 1;
+
+// date variubles
         String startDay="";
         String endDay="";
 
+// CSV data variubles
+        String station;
+        String lat;
+        String lon;
+        String elev;
+        String precip;
+        String tempLow;
+        String tempHigh;
+
+// temperature variubles
         int avgLow=0;
         int avgHigh=0;
         int dayHigh=0;
@@ -27,26 +40,35 @@ public class weatherData{
         for( int i=0; i < 35; i++) {
             System.out.println("Index of "+i+" : "+val[i]);
         }
-//*/
-///*
-        int lineNumber = 1;
+
+// traverse line by line throuhg the file
         while(fscaner.hasNextLine()){
+
+// set the line to be the text from the file
             line = fscaner.nextLine();
+// split the line using commas, gotta love standards
             val = line.split(",");
-            String station = val[0];
-            String lat = val[2];
-            String lon = val[3];
-            String elev= val[4];
-            String precip= val[7];
-            String tempLow = val[21].replaceAll("\"","");
-            String tempHigh= val[17].replaceAll("\"","");
+            lineNumber++;
+// get data from the line number
+            station = val[0];
+            lat = val[2];
+            lon = val[3];
+            elev= val[4];
+            precip= val[7];
+// remove the " so the temperature values can be transformed properly
+            tempLow = val[21].replaceAll("\"","");
+            tempHigh= val[17].replaceAll("\"","");
+
+// handle and track missing data for highs and lows
             if(tempHigh==""){
+            // use the averages from the days prior to that day to get an aproximaion
                 dayHigh=avgHigh/(lineNumber%7);
                 missingData=missingData+1;
             } else {
                 dayHigh=Integer.valueOf(tempHigh);
             }
             if(tempLow==""){
+            // use the averages from the days prior to that day to get an aproximaion
                 dayLow=avgLow/(lineNumber%7);            
                 missingData=missingData+1;
             } else {
@@ -56,18 +78,21 @@ public class weatherData{
             avgLow=avgLow+dayLow;
             avgHigh=avgHigh+dayHigh;
 
-            lineNumber++;
+
             if(lineNumber%7==0){
 //                System.out.println("Line : "+lineNumber);
+                // print out the seven day averages
                 System.out.println("7 day average for "+startDay+" to "+endDay+" the average highs and lows were "+avgHigh/7+" / "+avgLow/7);
+                // print out a warning about missing data.
                 if(missingData>0){
-                    System.out.println("Missing datapoints: "+missingData);
+                    System.out.println("# Averages may not be accurate, they were calculated based off of the previous days average");
+                    System.out.println("# Number of missing datapoints: "+missingData);
                 }
+                // reset the value counters
                 avgLow=0;
                 avgHigh=0;
-
                 missingData=0;
-
+                // set the dates
                 endDay=startDay;
                 startDay=val[6].replaceAll("\"","");
             }
